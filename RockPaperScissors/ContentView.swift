d3c104fd1de1f90350dfec93801dd4862e95ccb6
Erik Mikac <mikacew@gmail.com>
2020-02-16 10:48:42 -0500

    Added Draw condition and RPS conditional logic.

--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -11,26 +11,55 @@ import SwiftUI
 struct ContentView: View {
     @State private var rps = ["rock", "paper", "scissors"]
     @State private var cpuCurrentChoice = 0
-    @State private var usrShouldWin = Bool.random()
-    @State private var userSelection = ""
+    @State private var usrShouldWin = true
+    @State private var userSelection = 0
+    @State private var score = 0
+    @State private var isDraw = false
     var body: some View {
         VStack{
             HStack {
-                ForEach(0..<rps.count) { number in
+                ForEach(0..<rps.count, id: \.self) { number in
                         Button(action:
-                            {self.btnTapped()}) {
-                                Text("\(self.rps[number])")
+                        {self.btnTapped(number: number)}) {
+                            Text("\(self.rps[number])")
                         }
                     }.foregroundColor(.red).padding()
-                Spacer()
             }
-            HStack {
-                 Text("The CPU Choice: \(rps[cpuCurrentChoice])")
+            VStack {
+                Text("Your Choice: \(self.rps[userSelection])")
+                Text("CPU Choice: \(self.rps[cpuCurrentChoice])")
+                Text(isDraw ? "Draw!" : usrShouldWin ? "You Win" : "You Lose")
+                Text("Score: \(score)")
             }
         }
     }
-    func btnTapped() {
+    func btnTapped(number: Int) {
+        self.isDraw = false
         cpuCurrentChoice = Int.random(in: 0..<rps.count)
+        userSelection = number
+        if(userSelection == 0 && cpuCurrentChoice == 1) {
+            // User picked rock, CPU picked paper
+            self.usrShouldWin = false;
+            score -= 1
+        } else if(userSelection == 1 && cpuCurrentChoice == 0) {
+            self.usrShouldWin = true
+            score += 1
+        } else if(userSelection == 0 && cpuCurrentChoice == 2) {
+            self.usrShouldWin = true
+        } else if(userSelection == 2 && cpuCurrentChoice == 0) {
+            self.usrShouldWin = false
+            score -= 1
+        } else if(userSelection == 1 && cpuCurrentChoice == 2) {
+            self.usrShouldWin = false
+            score -= 1
+        } else if(userSelection == 2 && cpuCurrentChoice == 1) {
+            self.usrShouldWin = true
+            score += 1
+        } else {
+            self.usrShouldWin = false
+            self.isDraw = true
+            
+        }
         
     }
 
